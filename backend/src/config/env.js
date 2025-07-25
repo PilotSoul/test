3b3a9f1b7816bef59191f926b60286a837dbf0f6
Adelina Gamaiunova <theademari@gmail.com>
2025-07-25 10:13:16 +0500

    init

---
+++ backend/src/config/env.js
@@ -0,0 +1,34 @@
+
+const env = {
+  PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
+  CSRF_TOKEN_SECRET: process.env.CSRF_TOKEN_SECRET,
+  JWT_ACCESS_TOKEN_TIME_IN_MS: parseInt(
+    process.env.JWT_ACCESS_TOKEN_TIME_IN_MS,
+    10
+  ),
+  JWT_REFRESH_TOKEN_TIME_IN_MS: parseInt(
+    process.env.JWT_REFRESH_TOKEN_TIME_IN_MS,
+    10
+  ),
+  CSRF_TOKEN_TIME_IN_MS: parseInt(process.env.CSRF_TOKEN_TIME_IN_MS, 10),
+  
+  MAIL_FROM_USER: process.env.MAIL_FROM_USER,
+  EMAIL_VERIFICATION_TOKEN_TIME_IN_MS: parseInt(
+    process.env.EMAIL_VERIFICATION_TOKEN_TIME_IN_MS,
+    10
+  ),
+  PASSWORD_SETUP_TOKEN_TIME_IN_MS: parseInt(
+    process.env.PASSWORD_SETUP_TOKEN_TIME_IN_MS,
+    10
+  ),
+  PASSWORD_SETUP_TOKEN_SECRET: process.env.PASSWORD_SETUP_TOKEN_SECRET,
+  UI_URL: process.env.UI_URL,
+  API_URL: process.env.API_URL,
+  COOKIE_DOMAIN: process.env.COOKIE_DOMAIN,
+  RESEND_API_KEY: process.env.RESEND_API_KEY,
+};
+
+module.exports = { env };
